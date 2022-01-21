c2c88cfde0289b937bbfaa8d8bb4817a9688f75b
Kat <kmmpatke@gmail.com>
2022-01-20 18:59:38 -0600

    finish squared sorted array two pointer

--- src/main/java/educative/codinginterviewpatterns/twopointers/SquareSortedArray.java
+++ src/main/java/educative/codinginterviewpatterns/twopointers/SquareSortedArray.java
@@ -2,9 +2,13 @@ package educative.codinginterviewpatterns.twopointers;
 
 import shared.PracticeStatus;
 
+import java.util.Arrays;
+
 public class SquareSortedArray {
-  PracticeStatus practiceStatus = PracticeStatus.IN_PROGRESS;
+  PracticeStatus practiceStatus = PracticeStatus.ACCEPTED;
   String source = "https://www.educative.io/courses/grokking-the-coding-interview/R1ppNG3nV9R";
+  String timeComplexity = "O(n)";
+  String spaceComplexity = "O(n)";
   /*
   Given a sorted array, create a new array containing squares of all
   the numbers of the input array in the sorted order.
@@ -15,19 +19,17 @@ public class SquareSortedArray {
     int[] squares = new int[arr.length];
     int left = 0, right = arr.length - 1, sqIndex = arr.length - 1;
     int leftSq = 0, rightSq = 0;
-    while (left != right) {
-      leftSq = arr[left] * arr[left];
-      rightSq = arr[right] * arr[right];
-      if (rightSq > leftSq) {
-        squares[sqIndex] = rightSq;
-        squares[sqIndex - 1] = leftSq;
-      } else {
-        squares[sqIndex] = leftSq;
-        squares[sqIndex - 1] = rightSq;
-      }
-      sqIndex -= 2;
-      left++;
-      right--;
+    while (sqIndex >= 0) {
+        leftSq = arr[left] * arr[left];
+        rightSq = arr[right] * arr[right];
+        if (rightSq > leftSq) {
+          squares[sqIndex] = rightSq;
+          right--;
+        } else {
+          squares[sqIndex] = leftSq;
+          left++;
+        }
+      sqIndex -= 1;
     }
     return squares;
   }
--- src/test/java/educative/codinginterviewpatterns/twopointers/SquareSortedArrayTest.java
+++ src/test/java/educative/codinginterviewpatterns/twopointers/SquareSortedArrayTest.java
@@ -22,7 +22,10 @@ public class SquareSortedArrayTest {
   static Stream<Arguments> testCases() {
     return Stream.of(
         arguments(new int[] {-2, -1, 0, 2, 3}, new int[]{0, 1, 4, 4, 9}),
-        arguments(new int[] {-3, -1, 0, 1, 2}, new int[]{0, 1, 1, 4, 9})
+        arguments(new int[] {-3, -1, 0, 1, 2}, new int[]{0, 1, 1, 4, 9}),
+        arguments(new int[] {-7, 1, 4, 7, 8}, new int[]{1, 16, 49, 49, 64}),
+        arguments(new int[] {3, 10, 12}, new int[]{9, 100, 144}),
+        arguments(new int[] {2, 3, 4, 5}, new int[]{4, 9, 16, 25})
     );
   }
 }
