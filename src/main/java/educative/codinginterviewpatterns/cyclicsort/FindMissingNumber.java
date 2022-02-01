a41ebf24e94befe07ee0bf7ddbdb322bcf9484ea
Kat <kmmpatke@gmail.com>
2022-02-01 17:46:44 -0600

    missing number setup

---
+++ src/main/java/educative/codinginterviewpatterns/cyclicsort/FindMissingNumber.java
@@ -0,0 +1,15 @@
+package educative.codinginterviewpatterns.cyclicsort;
+
+import shared.PracticeStatus;
+
+public class FindMissingNumber {
+  String source = "https://www.educative.io/courses/grokking-the-coding-interview/JPnp17NYXE9";
+  PracticeStatus practiceStatus = PracticeStatus.IN_PROGRESS;
+  /*
+  * We are given an array containing ‘n’ distinct numbers taken from the range 0 to ‘n’.
+  * Since the array has only ‘n’ numbers out of the total ‘n+1’ numbers, find the missing number.
+  * */
+  int solution(int[] nums) {
+    return 0;
+  }
+}
---
+++ src/test/java/educative/codinginterviewpatterns/cyclicsort/FindMissingNumberTest.java
@@ -0,0 +1,28 @@
+package educative.codinginterviewpatterns.cyclicsort;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class FindMissingNumberTest {
+  FindMissingNumber findMissingNumber = new FindMissingNumber();
+
+  @ParameterizedTest
+  @MethodSource("testCases")
+  void testSolution(int[] inputNums, int expected) {
+    var actual = findMissingNumber.solution(inputNums);
+    assertEquals(expected, actual);
+  }
+
+  private static Stream<Arguments> testCases() {
+    return Stream.of(
+        arguments(new int[]{4, 0, 3, 1}, 2),
+        arguments(new int[]{8, 3, 5, 2, 4, 6, 0, 1}, 7)
+    );
+  }
+}
