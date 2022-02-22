38321a13378f170f57ded8067ac8d6900b0f3417
Kat <kmmpatke@gmail.com>
2022-02-22 17:06:16 -0600

    course scheduling

---
+++ src/main/java/leetcode/practice/CourseSchedule.java
@@ -0,0 +1,85 @@
+package leetcode.practice;
+
+import shared.PracticeStatus;
+
+import java.util.*;
+
+public class CourseSchedule {
+  String source = "https://leetcode.com/problems/course-schedule-ii/";
+  PracticeStatus practiceStatus = PracticeStatus.IN_PROGRESS;
+  String timeComplexity = "";
+  String spaceComplexity = "";
+
+  /*
+  * There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
+  * You are given an array prerequisites where prerequisites[i] = [a, b] indicates that you must take
+  * course b first if you want to take course a.
+
+  * For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+  * Return the ordering of courses you should take to finish all courses. If there are many valid
+  * answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+  Input: numCourses = 2, prerequisites = [[1,0]]
+  Output: [0,1]
+  * Explanation: There are a total of 2 courses to take. To take course 1 you should have finished
+  * course 0. So the correct course order is [0,1].
+  *
+  * 0: [n,n,2],
+  * 1: [0,n,2],
+  * 2: [0,1,n]
+  * */
+
+  int[] solution(int numCourses, int[][] prerequisites) {
+    if (prerequisites == null || prerequisites.length == 0) return new int[]{};
+    Map<Integer, List<Integer>> courses = new HashMap<>();
+    List<List<Integer>> result = new ArrayList<>();
+    for (int[] pair : prerequisites) {
+      int courseId = pair[0];
+      int prereq = pair[1];
+      List<Integer> currentPrereqs = courses.getOrDefault(courseId, new ArrayList<>());
+      currentPrereqs.add(prereq);
+      courses.put(courseId, currentPrereqs);
+    }
+    System.out.println(courses);
+    findCourseProgression(new ArrayList<>(), result, prerequisites[0][0], courses, new ArrayList<>(), numCourses);
+    return result.isEmpty()
+      ? new int[]{}
+      : result.get(0).stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  void findCourseProgression(List<Integer> courseOrder, List<List<Integer>> results, int courseId,
+                             Map<Integer, List<Integer>> courses, List<Integer> alreadySeen, int numCourses) {
+    System.out.println("-".repeat(15));
+    // if next progression is null (end of prereq chain) or equal to numCourses + 1 (checked all possible courses)
+    // add to result (along with any unaccounted for classes) and return
+    // if the node is present in alreadyVisited (is a cycle), don't add to result and return
+    // else loop through the prereqs and start new findCourseProgression call for each
+    // add to alreadyVisited list
+    System.out.println("findCourseProgression(): courseId " + courseId);
+    System.out.println("alreadySeen: " + alreadySeen);
+    if (!courses.containsKey(courseId)) {
+      courseOrder.add(0, courseId);
+      if (courseOrder.size() < numCourses) {
+        for (int i = 0; i < numCourses; i++) {
+          if (!courseOrder.contains(i)) {
+            courseOrder.add(i);
+          }
+        }
+        System.out.println("courseOrder: " + courseOrder);
+        results.add(courseOrder);
+      }
+    } else if (alreadySeen.contains(courseId)) {
+      // found a cycle
+      System.out.println("cycle—courseOrder: " + courseOrder);
+      return;
+    } else {
+      alreadySeen.add(courseId);
+      System.out.println("courseOrder: " + courseOrder);
+      for (Integer prereq : courses.get(courseId)) {
+        findCourseProgression(
+            new ArrayList<>(courseOrder),
+            results, prereq, courses, alreadySeen, numCourses);
+      }
+    }
+  }
+}
---
+++ src/test/java/leetcode/practice/CourseScheduleTest.java
@@ -0,0 +1,55 @@
+package leetcode.practice;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+public class CourseScheduleTest {
+  CourseSchedule courseSchedule = new CourseSchedule();
+
+  @ParameterizedTest
+  @MethodSource("testCases")
+  void testSolution(int numCourses, int[][] prerequisites, List<int[]> expected) {
+    var actual = courseSchedule.solution(numCourses, prerequisites);
+    System.out.print("Expected: ");
+    expected.forEach(arr -> System.out.println(Arrays.toString(arr)));
+    System.out.println("actual: " + Arrays.toString(actual));
+    assertActualAppearsInExpected(expected, actual);
+  }
+
+  void assertActualAppearsInExpected(List<int[]> expected, int[] actual) {
+    boolean foundMatch = false;
+    for (int[] solution : expected) {
+      if (solution.length != actual.length) {
+        continue;
+      }
+      int i = 0;
+      while (i < solution.length) {
+        if (solution[i] != actual[i]) {
+          continue;
+        }
+        i++;
+      }
+      foundMatch = true;
+    }
+    assertTrue(foundMatch);
+  }
+
+  static Stream<Arguments> testCases() {
+    return Stream.of(
+        arguments(2, new int[][]{new int[]{1,0}}, List.of(new int[] {0,1})),
+        arguments(2, new int[][]{new int[]{1,0}, new int[]{0,1}}, List.of(new int[] {})),
+        arguments(4, new int[][]{new int[]{1,0},new int[]{2,0},new int[]{3,1},new int[]{3,2}},
+            List.of(new int[]{0,2,1,3}, new int[]{0,1,2,3}))
+//        arguments(1, new int[][]{}, List.of(new int[]{0}))
+//        arguments(5, new int[][]{new int[]{1,0},new int[]{2,0},new int[]{3,1},new int[]{3,2}})
+    );
+  }
+}
