74a44f30f703286709dbc5902545af73e24bc3eb
Kat <kmmpatke@gmail.com>
2022-03-14 15:34:06 -0500

    unique ints sum to zero

--- src/main/java/leetcode/practice/UniqueIntsToZero.java
+++ src/main/java/leetcode/practice/UniqueIntsToZero.java
@@ -8,10 +8,24 @@ public class UniqueIntsToZero {
   String timeComplexity = "";
   String spaceComplexity = "";
   /*
-  * Given an integer n, return any array containing n unique integers such that they add up to 0.
-  * */
+   * Given an integer n, return any array containing n unique integers such that they add up to 0.
+   * */
 
-  int[] solution(int input) {
-    return new int[]{};
+  int[] solution(int n) {
+    if (n == 0) return new int[]{};
+    else if (n == 1) return new int[]{0};
+    int[] result = new int[n];
+    int midpoint = n / 2;
+    int left = midpoint - 1;
+    int right = n % 2 != 0 ? midpoint + 1 : midpoint;
+    int val = 1;
+    while (right < n) {
+      result[right] = val;
+      result[left] = val * -1;
+      right++;
+      left--;
+      val++;
+    }
+    return result;
   }
 }
--- src/test/java/leetcode/practice/UniqueIntsToZeroTest.java
+++ src/test/java/leetcode/practice/UniqueIntsToZeroTest.java
@@ -1,32 +1,53 @@
 package leetcode.practice;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.*;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class UniqueIntsToZeroTest {
   UniqueIntsToZero clazz = new UniqueIntsToZero();
 
   @ParameterizedTest
   @ValueSource(ints = {2, 5, 10})
-  void testSolution(int input, int[] expected) {
-    assertEquals(input, expected.length);
-    assertTrue(validateResult(clazz.solution(input)));
+  void testSolution(int input) {
+    var actual = clazz.solution(input);
+    System.out.println(Arrays.toString(actual));
+    assertEquals(input, actual.length);
+    assertTrue(validateResult(actual));
   }
 
   boolean validateResult(int[] input) {
-      Map<Integer, Integer> resultMap = new HashMap<>();
-      for (int val : input) {
-        resultMap.put(val, resultMap.getOrDefault(val, 0) + 1);
-      }
-      var hasNoDups = resultMap.entrySet().stream().noneMatch(entry -> entry.getValue() > 1);
-      var sumIsZero = Arrays.stream(input).sum() == 0;
-      return hasNoDups && sumIsZero;
+    Map<Integer, Integer> resultMap = new HashMap<>();
+    for (int val : input) {
+      resultMap.put(val, resultMap.getOrDefault(val, 0) + 1);
     }
+    var hasNoDups = resultMap.entrySet().stream().allMatch(entry -> entry.getValue() == 1);
+    var sumIsZero = Arrays.stream(input).sum() == 0;
+    return hasNoDups && sumIsZero;
+  }
+
+  @ParameterizedTest
+  @MethodSource("validationTestCases")
+  void validateTheValidation(int[] input, boolean expected) {
+    assertEquals(expected, validateResult(input));
+  }
+
+  static Stream<Arguments> validationTestCases() {
+    return Stream.of(
+        arguments(new int[]{1, 2, -1, -2, 0}, true),
+        arguments(new int[]{1, 1, -2, 3, -3}, false),
+        arguments(new int[]{4, 1, -2, 3, -3}, false)
+    );
+  }
 
 }
