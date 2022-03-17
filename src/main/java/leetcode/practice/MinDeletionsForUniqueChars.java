12057da631271920169113295d75c7b1451e1065
Kat <kmmpatke@gmail.com>
2022-03-16 19:52:34 -0500

    min deletions for unique char count - heap solution

--- src/main/java/leetcode/practice/MinDeletionsForUniqueChars.java
+++ src/main/java/leetcode/practice/MinDeletionsForUniqueChars.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class MinDeletionsForUniqueChars {
   String source = "https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/";
   PracticeStatus practiceStatus = PracticeStatus.ACCEPTED;
-  String timeComplexity = "O(N + K^2)"; // K is the maximum possible number of distinct characters in s
+  String timeComplexity = "Brute force: O(N + K^2), heap: O(n + K^2 log K)"; // K is the maximum possible number of distinct characters in s
   String spaceComplexity = "O(K)";
   /*
 A string s is called good if there are no two different characters in s that have the same frequency.
@@ -36,6 +36,32 @@ s contains only lowercase English letters.
         deletionCount++;
       }
       if (freqCount > 0) frequenciesPresent.add(freqCount);
+    }
+    return deletionCount;
+  }
+
+
+  int heapSolution(String s) {
+    if (s == null || s.length() == 0) return 0;
+    int deletionCount = 0;
+    Map<Character, Integer> charCount = new HashMap<>();
+    // needs to be a max heap, not min heap, since we're decrementing the counts as we go.
+    // with a min heap, the smaller counts fall off first and decrementing the counts isn't complete
+    PriorityQueue<Integer> frequencies = new PriorityQueue<>((a,b) -> b - a);
+    for (int i = 0; i < s.length(); i++) {
+      char currChar = s.charAt(i);
+      charCount.compute(currChar, (key, val) -> val == null ? 1 : val + 1);
+    }
+    frequencies.addAll(charCount.values());
+    if (frequencies.isEmpty()) return deletionCount;
+
+    while (frequencies.size() > 1) {
+      int minFrequency = frequencies.poll();
+      if (minFrequency == frequencies.peek()) {
+        deletionCount++;
+        minFrequency--;
+        if (minFrequency > 0) frequencies.add(minFrequency);
+      }
     }
     return deletionCount;
   }
--- src/test/java/leetcode/practice/MinDeletionsForUniqueCharsTest.java
+++ src/test/java/leetcode/practice/MinDeletionsForUniqueCharsTest.java
@@ -10,24 +10,30 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class MinDeletionsForUniqueCharsTest {
-    MinDeletionsForUniqueChars clazz = new MinDeletionsForUniqueChars();
+  MinDeletionsForUniqueChars clazz = new MinDeletionsForUniqueChars();
 
-    @ParameterizedTest
-    @MethodSource("testcases")
-    void testSolution(String input, int expected) {
-      assertEquals(expected, clazz.solution(input));
-    }
-
-    static Stream<Arguments> testcases() {
-      return Stream.of(
-          arguments("aab", 0),
-          arguments("aaabbbcc", 2),
-          arguments("ceabaacb", 2),
-          arguments("abbccccdddd", 1),
-          arguments("aabbcc", 3),
-          arguments("aabbccdd", 5),
-          arguments("addabbececcddebbe", 7)
-      );
-    }
-
+  @ParameterizedTest
+  @MethodSource("testcases")
+  void testSolution(String input, int expected) {
+    assertEquals(expected, clazz.solution(input));
   }
+
+  @ParameterizedTest(name = "Heap approach {index}")
+  @MethodSource("testcases")
+  void testHeapSolution(String input, int expected) {
+    assertEquals(expected, clazz.heapSolution(input));
+  }
+
+  static Stream<Arguments> testcases() {
+    return Stream.of(
+        arguments("aab", 0),
+        arguments("aaabbbcc", 2),
+        arguments("ceabaacb", 2),
+        arguments("abbccccdddd", 1),
+        arguments("aabbcc", 3),
+        arguments("aabbccdd", 5),
+        arguments("addabbececcddebbe", 7)
+    );
+  }
+
+}
